4362c1efd84fa78b7564f77b06dfb9b817b66182
CheesyLasagna <57880001+CheesyLasagna@users.noreply.github.com>
2019-11-25 09:55:48 +0800

    Add files via upload

---
+++ JavaApplication1/src/Javaapplication.java
@@ -0,0 +1,23 @@
+public class Javaapplication 
+{
+    public static void main(String[] args) 
+    {
+        for(int i=4; i >= 1; --i)
+        {
+            for(int j = 1; j <= i; ++j)
+            {
+            System.out.print("* ");
+            }
+            System.out.println();
+        }
+         for(int x = 1; x <= 4; ++x) 
+         {
+            for(int y = 1; y <= x; ++y)
+            {
+                System.out.print("** ");
+            }
+            System.out.println();
+            }
+    } 
+     
+}
